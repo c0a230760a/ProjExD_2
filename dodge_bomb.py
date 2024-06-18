fc7c6eae258ccf8aa2f2b41ddb20ef71f9f7052b
shuto <c0a230760a@edu.teu.ac.jp>
2024-06-18 14:23:51 +0900

    練習2:爆弾サーフェイスの作成と移動

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,4 +1,5 @@
 import os
+import random
 import sys
 import pygame as pg
 
@@ -19,6 +20,12 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 2.0)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 900, 400
+    enn = pg.Surface((20, 20)) #　一辺が20のからのサーフェイスを作る
+    enn.set_colorkey((0, 0, 0))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn_rct = enn.get_rect()
+    enn_rct.center = random.randint(0, WIDTH), random.randint(0, HEIGHT)
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -35,6 +42,8 @@ def main():
                 sum_mv[1] += v[1]
         kk_rct.move_ip(sum_mv) # 重要
         screen.blit(kk_img, kk_rct)
+        enn_rct.move_ip(vx, vy)
+        screen.blit(enn, enn_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
